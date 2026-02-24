2c1920a79ce17ceb7dbd3c263e96d71d1faf488f
j-marshall124 <jordan.marshall1@mohawkcollege.ca>
2026-02-24 17:13:15 -0500

    - add drawing eyeball

--- vector-eyeball/Game.cs
+++ vector-eyeball/Game.cs
@@ -18,7 +18,8 @@ namespace MohawkGame2D
         /// </summary>
         public void Setup()
         {
-
+            Window.SetTitle("Eyeball Animation with Vectors");
+            Window.SetSize(400, 400);
         }
 
         /// <summary>
@@ -26,7 +27,25 @@ namespace MohawkGame2D
         /// </summary>
         public void Update()
         {
+            Window.ClearBackground(Color.OffWhite);
+            DrawEyeball(Window.Size / 2);
+        }
 
+        void DrawEyeball(Vector2 position)
+        {
+            // Cornea
+            Draw.LineColor = Color.Black;
+            Draw.LineSize = 1;
+            Draw.FillColor = Color.White;
+            Draw.Circle(position, 50);
+
+            // Iris
+            Draw.FillColor = Color.Gray;
+            Draw.Circle(position, 35);
+
+            // Pupil
+            Draw.FillColor = Color.Black;
+            Draw.Circle(position, 20);
         }
     }
 
