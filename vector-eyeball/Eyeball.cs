3647e7d4f54e477877f126b7eaac5a1860d0e3bc
j-marshall124 <jordan.marshall1@mohawkcollege.ca>
2026-03-05 15:45:17 -0500

    Transferred variables and created eyeball array

---
+++ vector-eyeball/Eyeball.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace MohawkGame2D
+{
+    public class Eyeball
+    {
+        public Vector2 eyePosition;
+        public float radius;
+        Color color;
+
+        // Constructor
+        public Eyeball() 
+        {
+            // Put it anywhere on screen
+            eyePosition = Random.Vector2(Window.Size);
+            // Give random size
+            radius = Random.Integer(15, 50);
+            color = Random.Color();
+        }
+
+        public Eyeball(float x, float y, float radius)
+        {
+            this.eyePosition = new Vector2(x, y);
+            this.radius = radius;
+        }
+
+        public void DrawEyeball()
+        {
+            // Calculate ratios for each eye
+            float corneaR = radius;
+            float irisR = radius * 0.7f; // 0.7 = 70%
+            float pupilR = radius * 0.3f; // 0.3 == 30%
+
+            // Cornea
+            Draw.LineColor = Color.Black;
+            Draw.LineSize = 1;
+            Draw.FillColor = Color.White;
+            Draw.Circle(eyePosition, corneaR);
+
+            // Set up our look vector
+            // To go from point A to point B, we do B - A
+            Vector2 mousePosition = Input.GetMousePosition();
+            Vector2 vectorFromEyeToMouse = mousePosition - eyePosition;
+
+            // Split vector into it's 2 components: direction and magnitude
+            Vector2 direction = Vector2.Normalize(vectorFromEyeToMouse);
+            float distance = vectorFromEyeToMouse.Length();
+
+            //Calculate where to position iris and pupil
+            Vector2 irisPupilPosition;
+            float maxMoveDistance = corneaR - irisR;
+            bool isInsideEye = distance < maxMoveDistance;
+            if (isInsideEye == true)
+            {
+                irisPupilPosition = mousePosition;
+            }
+            else // is outside eye
+            {
+                irisPupilPosition = eyePosition + direction * maxMoveDistance;
+            }
+
+            // Iris
+            Draw.FillColor = color;
+            Draw.Circle(irisPupilPosition, irisR);
+
+            // Pupil
+            Draw.FillColor = Color.Black;
+            Draw.Circle(irisPupilPosition, pupilR);
+        }
+    }
+}
--- vector-eyeball/Game.cs
+++ vector-eyeball/Game.cs
@@ -5,82 +5,30 @@ using System.Numerics;
 // The namespace your code is in.
 namespace MohawkGame2D
 {
-    /// <summary>
-    ///     Your game code goes inside this class!
-    /// </summary>
     public class Game
     {
-        // Place your variables here:
-        Vector2[] positions = [new (200, 200), new (100, 100), new (300, 300)];
-        float[] radii = [50, 10, 35];
+        Eyeball[] eyeballs = new Eyeball[]
+        {
+            new Eyeball(),
+            new Eyeball(200, 200, 50),
+            new Eyeball(100, 100, 35),
+            new Eyeball(100, 300, 15),
+        };
 
-        /// <summary>
-        ///     Setup runs once before the game loop begins.
-        /// </summary>
+
         public void Setup()
         {
             Window.SetTitle("Eyeball Animation with Vectors");
             Window.SetSize(400, 400);
         }
 
-        /// <summary>
-        ///     Update runs every frame.
-        /// </summary>
         public void Update()
         {
             Window.ClearBackground(Color.OffWhite);
-
-            for (int i = 0; i < positions.Length; i++)
+            for (int i = 0; i < eyeballs.Length; i++)
             {
-                Vector2 position = positions[i];
-                float radius = radii[i];
-                DrawEyeball(position, radius);
+                eyeballs[i].DrawEyeball();
             }
-        }
-
-        void DrawEyeball(Vector2 eyePosition, float radius)
-        {
-            // Calculate ratios for each eye
-            float corneaR = radius;
-            float irisR = radius * 0.7f; // 0.7 = 70%
-            float pupilR = radius * 0.3f; // 0.3 == 30%
-
-            // Cornea
-            Draw.LineColor = Color.Black;
-            Draw.LineSize = 1;
-            Draw.FillColor = Color.White;
-            Draw.Circle(eyePosition, corneaR);
-
-            // Set up our look vector
-            // To go from point A to point B, we do B - A
-            Vector2 mousePosition = Input.GetMousePosition();
-            Vector2 vectorFromEyeToMouse = mousePosition - eyePosition;
-
-            // Split vector into it's 2 components: direction and magnitude
-            Vector2 direction = Vector2.Normalize(vectorFromEyeToMouse);
-            float distance = vectorFromEyeToMouse.Length();
-
-            //Calculate where to position iris and pupil
-            Vector2 irisPupilPosition;
-            float maxMoveDistance = corneaR - irisR;
-            bool isInsideEye = distance < maxMoveDistance;
-            if (isInsideEye == true)
-            {
-                irisPupilPosition = mousePosition;
-            }
-            else // is outside eye
-            {
-                irisPupilPosition = eyePosition + direction * maxMoveDistance;
-            }
-
-            // Iris
-            Draw.FillColor = Color.Gray;
-            Draw.Circle(irisPupilPosition, irisR);
-
-            // Pupil
-            Draw.FillColor = Color.Black;
-            Draw.Circle(irisPupilPosition, pupilR);
         }
     }
-
 }
